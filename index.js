c8565b1de214aeec9c6fdf4b99dacaa33635a88e
k_krylowicz <kacper@krylowicz.pl>
2020-02-01 12:26:20 +0100

    :sparkles: fetching followers ids

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const OAuth = require('oauth');
+
+const oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  process.env.CONSUMER_KEY,
+  process.env.CONSUMER_SECRET,
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+);
+
+oauth.get(
+  'https://api.twitter.com/1.1/followers/ids.json',
+  process.env.ACCESS_TOKEN,
+  process.env.ACCESS_SECRET,
+  (error, data) => {
+    if (error) console.log(error);
+    const { ids } = JSON.parse(data);
+    console.log(ids);
+  }
+);
