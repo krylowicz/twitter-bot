fb1bd9b18379ef8fa84759046ea1ca142674d5f8
k_krylowicz <kacper@krylowicz.pl>
2020-02-01 14:17:28 +0100

    :sparkles: 1st working version

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const fs = require('fs');
 const OAuth = require('oauth');
+
+const diff = (arr1, arr2) => arr1.filter(x => !arr2.includes(x));
 
 const oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
@@ -11,13 +14,44 @@ const oauth = new OAuth.OAuth(
   'HMAC-SHA1'
 );
 
-oauth.get(
+const fetchFollowers = () => oauth.get(
   'https://api.twitter.com/1.1/followers/ids.json',
   process.env.ACCESS_TOKEN,
   process.env.ACCESS_SECRET,
   (error, data) => {
     if (error) console.log(error);
-    const { ids } = JSON.parse(data);
-    console.log(ids);
+    console.log('fetched followers');
+    const { ids: newIDs } = JSON.parse(data);
+    const oldIDs = JSON.parse(fs.readFileSync('./followers.json'));
+    if (oldIDs) {
+      const difference = diff(newIDs, oldIDs);
+      for (let id of difference) {
+        oauth.post(
+          'https://api.twitter.com/1.1/direct_messages/events/new.json',
+          process.env.ACCESS_TOKEN,
+          process.env.ACCESS_SECRET,
+          JSON.stringify({
+            "event": {
+              "type": "message_create",
+              "message_create": {
+                "target": {
+                  "recipient_id": id
+                },
+                "message_data": {
+                  "text": "Thanks for following @bb_stem and becoming part of the network on Twitter. Our feed brings you all the latest news and updates about BBSTEM. If you wish to receive all the exclusive opportunities, sign up here: bbstem.co.uk"
+                }
+              }
+            }
+          }),
+          'application/json'
+        );
+        console.log(`message sent to ${id}`);
+      }
+    }
+    fs.writeFileSync('./followers.json', JSON.stringify(newIDs));
   }
 );
+
+fetchFollowers();
+
+setInterval(fetchFollowers, 60000);
